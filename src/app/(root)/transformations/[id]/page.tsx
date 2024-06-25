118c8ecbd8f83883114bc9a2568396382b2437c4
Your Name <theodryden@yahoo.com>
2024-06-25 10:18:13 +0100

    first commit

---
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>
+      TransformationsPage
+    </div>
+  )
+}
+
+export default TransformationsPage
+//Dyanmic routes uses square brackets in folders [id]
